25bb2132d895e29ae58c90b4612a98ad7e6fec97
Zac Alexander <zac@znacreative.com>
2021-03-10 18:14:21 +0000

    Added setupInput

--- play.js
+++ play.js
@@ -1,3 +1,14 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+// Setup interface, so we can handle user input with stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+setupInput();
