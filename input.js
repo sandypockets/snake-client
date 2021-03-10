511ac79cc124d990c2dd8c394cfa3f6dc8c0934b
Zac Alexander <zac@znacreative.com>
2021-03-10 20:48:08 +0000

    Reverted movement change bugs

--- input.js
+++ input.js
@@ -4,21 +4,13 @@ const handleUserInput = (key)=> {
     console.log('You exited the game.');
     process.exit();
   } else if (key === 'w') {
-      setInterval(() => {
         conn.write("Move: up");
-      }, 600);
     } else if (key === 'a') {
-      setInterval(() => {
         conn.write("Move: left");
-      }, 600);
     } else if (key === 's') {
-      setInterval(() => {
         conn.write("Move: down");
-      }, 600);
     } else if (key === 'd') {
-      setInterval(() => {
         conn.write("Move: right");
-      }, 600);
     }
   };
 
